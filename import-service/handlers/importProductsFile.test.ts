b0ec9f894213df082326dcec05030af1feb99689
Pavel Zaletski <Pavel.Zaletski@coxautoinc.com>
2022-10-31 12:09:01 +0400

    refactor (task 5): refactor code, update tests

--- import-service/handlers/importProductsFile.test.ts
+++ import-service/handlers/importProductsFile.test.ts
@@ -1,27 +1,32 @@
-// @ts-nocheck
-
-import * as AWSMock from 'aws-sdk-mock';
-import * as AWS from 'aws-sdk';
 import { importProductsFile } from './importProductsFile';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { headers } from '../constants';
+import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
+import { mock } from 'jest-mock-extended';
+
+jest.mock('@aws-sdk/s3-request-presigner', () => ({
+  getSignedUrl: jest.fn(),
+}));
+
+jest.mock('@aws-sdk/client-s3', () => ({
+  S3Client: jest.fn(),
+  PutObjectCommand: jest.fn(),
+}));
+
+const context = mock<Context>();
+const callback = mock<Callback>();
 
 describe('importProductsFile', () => {  
   it('should mock getSignedUrl from S3', async () => {
-    AWSMock.setSDKInstance(AWS);
     const mockSignedUrl = 'test';
-
-    AWSMock.mock('S3', 'getSignedUrl', (action, _params, callback) => {
-      console.log('S3', 'getSignedUrl', 'mock called');
-      callback(null, mockSignedUrl);
-    });
-
+    (getSignedUrl as jest.Mock).mockResolvedValueOnce(mockSignedUrl);
     const awsEvent = {
       queryStringParameters: {
         name: 'products.csv'
       },
     };
 
-    const response = await importProductsFile(awsEvent);
+    const response = await importProductsFile(awsEvent as unknown as  APIGatewayProxyEvent, context, callback);
 
     expect(response).toEqual({
       body: mockSignedUrl,
--- import-service/handlers/importProductsFile.ts
+++ import-service/handlers/importProductsFile.ts
@@ -1,37 +1,30 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import 'source-map-support/register';
-import * as AWS from 'aws-sdk';
 import { headers } from '../constants';
 
 const BUCKET = 'node-in-aws-s3-pz';
 
 export const importProductsFile: APIGatewayProxyHandler = async (event, _context) => {
   try {
-    const catalogName = event.queryStringParameters.name;
-    const catalogPath = `uploaded/${catalogName}`;
-
-    const s3 = new AWS.S3({ region: 'eu-west-1' });
+    const fileName = event.queryStringParameters.name;
+    const catalogPath = `uploaded/${fileName}`;
+    const s3 = new S3Client({ region: 'eu-west-1' });
     const params = {
       Bucket: BUCKET,
       Key: catalogPath,
-      Expires: 60,
       ContentType: 'text/csv',
     };
 
-    return new Promise((resolve, reject) => {
-      s3.getSignedUrl('putObject', params, (err, url) => {
-        console.log('err', err);
-        if (err) {
-          return reject(err);
-        }
-
-        resolve({
-          statusCode: 200,
-          headers: headers,
-          body: url,
-        });
-      });
-    });
+    const command = new PutObjectCommand(params);
+    const signedUrl = await getSignedUrl(s3, command, { expiresIn: 60 });
+        
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: signedUrl,
+    };
   } catch (err) {
     console.log('Server error - ', err);
     return {
